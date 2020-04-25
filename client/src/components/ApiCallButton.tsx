4e3ee599c08aaa928a8bba53f6c5cbc28a34af5a
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-04-25 20:24:54 +0100

    Add proxy and set up simple API request

---
+++ client/src/components/ApiCallButton.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+type Props = {};
+
+const ApiCallButton = (props: Props) => {
+    const handleApiCall = () => {
+        console.log("Yeah this was clicked");
+        fetch(`/api/kek`)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    };
+
+    return (
+        <>
+            <div>This is the API call</div>
+            <button onClick={handleApiCall}>
+                Click me and check the console
+            </button>
+        </>
+    );
+};
+
+export default ApiCallButton;
--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { SERVER_PORT } from "./utils/secrets";
 
 // Express configuration
@@ -8,9 +9,18 @@ app.set("server_port", SERVER_PORT);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// CORS config
+// TODO: Update CORS headers
+app.use(cors());
+app.options("*", cors());
+
 // test GET and serve
 app.get("/", (req, res) => {
     res.send("kek");
+});
+
+app.get("/api/kek", (req, res) => {
+    res.json({ kek: "top kek" });
 });
 
 export default app;
