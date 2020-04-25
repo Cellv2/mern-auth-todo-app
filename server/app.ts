4c524a137b811d244337a9b1c2f619963850000b
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-04-25 10:05:12 +0100

    Add super basic express get to make sure it works

--- server/app.ts
+++ server/app.ts
@@ -8,4 +8,9 @@ app.set("server_port", SERVER_PORT);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// test GET and serve
+app.get("/", (req, res) => {
+    res.send("kek");
+});
+
 export default app;
