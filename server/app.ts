adb1ad6835ffc97d800069fc6ddad1fec45b1c37
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-04-26 17:26:43 +0100

    :heavy_plus_sign: Adding body-parser

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 import cors from "cors";
 import { SERVER_PORT } from "./utils/secrets";
 import router from "./routes/createRouter";
@@ -9,6 +10,10 @@ app.set("server_port", SERVER_PORT);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // CORS config
 // TODO: Update CORS headers
