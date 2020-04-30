7197d66f9fdd20ba3bc66f6656616ff6b96dfc04
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-04-30 21:26:07 +0100

    :fire: Remove unrequired files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Layout from "./layouts/Layout";
-import ApiCallButton from "./components/ApiCallButton";
-import ToDoContainer from "./components/ToDoContainer";
 import Main from "./components/Main";
 
 import About from "./components/About";
 import User from "./components/User/User";
 import Login from "./components/User/Login";
 import Logout from "./components/User/Logout";
-
-import styles from "./App.module.scss";
 
 const App = () => {
     return (
