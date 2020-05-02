5d8a71a82d65bd5fd76ccebe720f6fa400d39994
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-05-02 21:55:13 +0100

    Add route to get all todos

--- server/routes/api/users/todos/getTodos.ts
+++ server/routes/api/users/todos/getTodos.ts
@@ -1,9 +1,16 @@
 import express, { Request, Response } from "express";
+import TodoCollection from "../../../../models/Todo/todo-collection.model";
 
 const router = express.Router();
 
 router.get("/users/todos", (req: Request, res: Response) => {
-    res.send("getTodos.ts");
+    TodoCollection.find((err, todo) => {
+        if (err) {
+            console.error(err);
+        }
+
+        res.json(todo);
+    });
 });
 
 export default router;
