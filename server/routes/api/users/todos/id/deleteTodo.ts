6ead33e0b50e246a5c3b58ade672b4fabfd991d0
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-05-03 14:19:13 +0100

    Add DB delete operation

--- server/routes/api/users/todos/id/deleteTodo.ts
+++ server/routes/api/users/todos/id/deleteTodo.ts
@@ -1,9 +1,27 @@
 import express, { Request, Response } from "express";
 
+import TodoCollection from "../../../../../models/Todo/todo-collection.model";
+
 const router = express.Router();
 
+const deleteTodo = (req: Request, res: Response): void => {
+    const id = req.params.id;
+    const query = { author: id };
+
+    TodoCollection.findOneAndDelete(query, (err, deleted) => {
+        if (err) {
+            console.error("There was a problem deleting the item", err);
+        }
+
+        res.statusCode = 200;
+        res.json(deleted);
+    });
+
+    return;
+};
+
 router.delete("/users/todos/:id", (req: Request, res: Response) => {
-    res.send("This is the delete route - the ID was " + req.params.id);
+    deleteTodo(req, res);
 });
 
 export default router;
