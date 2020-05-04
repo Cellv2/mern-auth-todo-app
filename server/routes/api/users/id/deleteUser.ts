ba9d0e6bdcb3db8c528df4e53b4563226ad74e4b
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-05-04 21:33:11 +0100

    Add user DELETE route

--- server/routes/api/users/id/deleteUser.ts
+++ server/routes/api/users/id/deleteUser.ts
@@ -1,9 +1,25 @@
 import express, { Router, Request, Response } from "express";
+import UserCollection from "../../../../models/User/user-collection.model";
 
 const router: Router = express.Router();
 
-router.get("/users/:id/deleteUser", (req: Request, res: Response) => {
-    res.json({ deleteUser: "deleteUser.ts" });
+const deleteUser = (req: Request, res: Response): void => {
+    const id: string = "" + req.params.id;
+
+    UserCollection.findByIdAndDelete(id, (err, deleted) => {
+        if (err) {
+            console.error(err);
+        }
+
+        res.statusCode = 200;
+        res.json(deleted);
+    });
+
+    return;
+};
+
+router.delete("/users/:id/deleteUser", (req: Request, res: Response) => {
+    deleteUser(req, res);
 });
 
 export default router;
