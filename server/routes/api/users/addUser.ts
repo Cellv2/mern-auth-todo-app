880ad3265db14e56be6802e9585b3afd92f0983b
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-05-06 23:00:00 +0100

    Add check for whether user email already exists

--- server/routes/api/users/addUser.ts
+++ server/routes/api/users/addUser.ts
@@ -10,22 +10,36 @@ const addUser = (req: Request, res: Response): void => {
 
     if (!isValid) {
         res.status(400).json(errors);
+
         return;
     }
 
-    const user = new UserCollection({
-        name: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        // todos: ["5eaf07d7bcdbc64d6851f536", "5eaf07d7bcdbc64d6851f537"],
-    });
+    // check whether email is already in use before registering
+    const email = req.body.email;
+    const query = { email: email };
+    UserCollection.findOne(query, (err, user) => {
+        if (user) {
+            res.status(400).json({ email: "Email is already in use" });
 
-    user.save((err) => {
-        if (err) {
-            console.log(err);
+            return;
         }
 
-        res.status(201).json(user);
+        const newUser = new UserCollection({
+            name: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            // todos: ["5eaf07d7bcdbc64d6851f536", "5eaf07d7bcdbc64d6851f537"],
+        });
+
+        newUser.save((err) => {
+            if (err) {
+                console.log(err);
+            }
+
+            res.status(201).json(newUser);
+        });
+
+        return;
     });
 
     return;
