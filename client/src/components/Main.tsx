2cca34772d9b0d27513be787679e166eaa35ed04
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-05-11 21:38:28 +0100

    Remove unrequired files

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -1,25 +1,3 @@
-// import React from "react";
-
-// import ApiCallButton from "./ApiCallButton";
-// import ToDoContainer from "./ToDoContainer";
-
-// import styles from "./Main.module.scss";
-
-// type Props = {};
-
-// const Main = (props: Props) => {
-//     return (
-//         <div className={styles.app}>
-//             <header className={styles.appHeader}>
-//                 <ApiCallButton />
-//                 <ToDoContainer />
-//             </header>
-//         </div>
-//     );
-// };
-
-// export default Main;
-
 import React, { Component } from 'react'
 
 import ToDoForm from "./ToDoForm";
