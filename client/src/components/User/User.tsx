e53f61ab5a8231fe42f6aaa4b8913b2409bf160f
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-05-13 21:16:09 +0100

    Add app state theme switching

--- client/src/components/User/User.tsx
+++ client/src/components/User/User.tsx
@@ -1,11 +1,37 @@
 import React from "react";
 
-type Props = {};
+import { ApplicationState } from "../../types/application-state-types";
+import { UpdateStateActions } from "../../types/state-action-types";
+import { AvailableThemes } from "../../types/theme-types";
+
+type Props = {
+    applicationState: ApplicationState;
+    handleAppStateUpdate: (
+        newState: ApplicationState,
+        actionToTake: UpdateStateActions
+    ) => void;
+};
 
 const User = (props: Props) => {
+    const handleOnClick = (theme: AvailableThemes) => {
+        console.log(theme);
+
+        let newState = props.applicationState;
+        newState.theme = theme;
+
+        props.handleAppStateUpdate(newState, "updateThemeState");
+    };
+
     return (
         <div>
             <h1>This is the user page</h1>
+            <p>The current theme is: {props.applicationState.theme}</p>
+            <button onClick={() => handleOnClick("dark")}>
+                Set dark theme
+            </button>
+            <button onClick={() => handleOnClick("light")}>
+                Set light theme
+            </button>
         </div>
     );
 };
