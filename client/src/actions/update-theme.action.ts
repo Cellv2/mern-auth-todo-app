3575bc7908ab26fd129162eecc85d5bd9d6e7003
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-05-14 19:48:39 +0100

    Comments

--- client/src/actions/update-theme.action.ts
+++ client/src/actions/update-theme.action.ts
@@ -5,6 +5,11 @@ export const updateThemeActions = {
     SET_THEME: "SET_THEME",
 };
 
+/**
+ * Function to merge state into the payload and return new state for themes
+ * @param {ApplicationState} state The current state to be updated and returned
+ * @param {StateAction} action Type: the update type to be performed, Payload: The state key:val to be merged into state
+ */
 export const updateTheme = (state: ApplicationState, action: StateAction) => {
     switch (action.type) {
         case updateThemeActions.SET_THEME:
