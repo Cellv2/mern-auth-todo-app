4d8a0d9053cfa9ae72f2c9ff9bacf318f59b4d11
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-05-15 22:01:24 +0100

    Add register portion of #7

--- server/models/User/user-collection.model.ts
+++ server/models/User/user-collection.model.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const Schema = mongoose.Schema;
 
@@ -13,6 +14,21 @@ const userSchema = new Schema({
         required: true,
     },
     todos: [{ type: Schema.Types.ObjectId, ref: "Todo" }],
+});
+
+userSchema.pre("save", function (next) {
+    if (this.isModified("password")) {
+        //@ts-expect-error
+        return bcrypt.hash(this.password, 10, (err, hash) => {
+            if (err) {
+                return next(err);
+            }
+
+            //@ts-expect-error
+            this.password = hash;
+            next();
+        });
+    }
 });
 
 const UserCollection = mongoose.model("User", userSchema);
