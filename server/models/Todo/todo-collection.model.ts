00f80c8fd050aaa059ca63a56e8af86ebec1c3fb
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-05-16 17:15:32 +0100

    Add todo schema definition

--- server/models/Todo/todo-collection.model.ts
+++ server/models/Todo/todo-collection.model.ts
@@ -2,11 +2,17 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
+export interface ToDoCollection extends mongoose.Document {
+    userid: string;
+    isComplete: boolean;
+    text: string
+}
+
 const todoSchema = new Schema({
     userid: String, // User._id
     isComplete: Boolean,
     text: String,
 });
 
-const TodoCollection = mongoose.model("Todo", todoSchema);
+const TodoCollection = mongoose.model<ToDoCollection>("Todo", todoSchema);
 export default TodoCollection;
