9e44b23135848ef432eec3e75994cbc2313d9036
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-05-18 21:59:20 +0100

    Update route to be more generic (#13)

--- server/routes/api/users/todos/getTodos.ts
+++ server/routes/api/users/todos/getTodos.ts
@@ -6,39 +6,6 @@ import TodoCollection from "../../../../models/Todo/todo-collection.model";
 
 const router: Router = express.Router();
 
-const getUserTodos_old = (req: Request, res: Response) => {
-    const id: string = req.params.id;
-    const query = { userid: id };
-
-    console.log(id);
-    console.log(req.headers.authorization);
-
-    const token = req.headers.authorization!.split(" ")[1];
-
-    jwt.verify(token, secretOrKey, (err, authorizedData) => {
-        if (err) {
-            console.error(err);
-            return;
-        } else {
-            console.log("yarr");
-        }
-    });
-
-    TodoCollection.find(query, (err, todos) => {
-        if (err) {
-            console.error(err);
-        }
-
-        res.json(todos);
-    });
-
-    return;
-};
-
-router.get("/users/:id/todos", (req: Request, res: Response) => {
-    getUserTodos_old(req, res);
-});
-
 const getUserTodos = (req: Request, res: Response) => {
     if (!req.headers.authorization) {
         res.sendStatus(401);
@@ -50,12 +17,12 @@ const getUserTodos = (req: Request, res: Response) => {
     jwt.verify(token, secretOrKey, (err, authorizedData) => {
         if (err) {
             console.log(err);
+            res.sendStatus(500);
 
             return;
         } else {
-            console.dir(authorizedData);
             if (!authorizedData) {
-                res.sendStatus(403);
+                res.sendStatus(401);
             } else {
                 //@ts-expect-error
                 const id: string = authorizedData.id;
@@ -79,7 +46,6 @@ const getUserTodos = (req: Request, res: Response) => {
 
 router.get("/user/todos", (req: Request, res: Response) => {
     getUserTodos(req, res);
-    // res.send("we hit this route")
 });
 
 export default router;
