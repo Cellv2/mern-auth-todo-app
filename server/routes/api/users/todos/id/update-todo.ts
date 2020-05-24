bfacad0cae22495f37634094bdcfb05308124c1f
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-05-24 21:19:05 +0100

    Add server side auth on update rodo

--- server/routes/api/users/todos/id/update-todo.ts
+++ server/routes/api/users/todos/id/update-todo.ts
@@ -1,26 +1,53 @@
 import express, { Router, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
 import TodoCollection from "../../../../../models/Todo/todo-collection.model";
+import { secretOrKey } from "../../../../../utils/secrets";
 
 const router: Router = express.Router();
 
 const updateTodo = (req: Request, res: Response) => {
-    const todoId: string = req.params.id;
-    const newData = req.body;
+    if (!req.headers.authorization) {
+        res.sendStatus(401);
 
-    TodoCollection.findByIdAndUpdate(
-        todoId,
-        newData,
-        { upsert: false, new: true },
-        (err, updated) => {
-            if (err) {
-                res.send(500);
-                console.error(err);
-            }
+        return;
+    }
 
-            res.statusCode = 200;
-            res.json(updated);
+    const token = req.headers.authorization.split(" ")[1];
+
+    jwt.verify(token, secretOrKey, (err, authorizedData) => {
+        if (err) {
+            res.sendStatus(500);
+
+            return;
         }
-    );
+
+        // ensure that the user actually matches the token
+        //@ts-expect-error
+        if (!authorizedData || authorizedData.id !== req.body.userid) {
+            res.sendStatus(401);
+
+            return;
+        } else {
+            const todoId: string = req.params.id;
+            const newData = req.body;
+
+            TodoCollection.findByIdAndUpdate(
+                todoId,
+                newData,
+                { upsert: false, new: true },
+                (err, updated) => {
+                    if (err) {
+                        res.send(500);
+                        console.error(err);
+                    }
+
+                    res.statusCode = 200;
+                    res.json(updated);
+                }
+            );
+        }
+    });
 
     return;
 };
