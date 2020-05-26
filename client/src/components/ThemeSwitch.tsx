55a940ddd3c923e08a3a181a8a513047d5e479cb
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-05-26 23:06:00 +0100

    Add theme switch clicking

--- client/src/components/ThemeSwitch.tsx
+++ client/src/components/ThemeSwitch.tsx
@@ -8,17 +8,29 @@ import styles from "./ThemeSwitch.module.scss";
 
 type Props = {};
 
+// TODO: Make nicer transition between the dark/light toggle
 const ThemeSwitch = (props: Props) => {
     const [theme, setTheme] = useState<AvailableThemes>("light");
+
+    const handleThemeSwitch = () => {
+        theme === "light" ? setTheme("dark") : setTheme("light");
+    };
+
     return (
-        <button className={styles.container}>
+        <button className={styles.container} onClick={handleThemeSwitch}>
             <FontAwesomeIcon
                 icon={faMoon}
                 className={styles.moon}
                 size={"3x"}
             />
             <FontAwesomeIcon icon={faSun} className={styles.sun} size={"3x"} />
-            <div className={`${styles.toggle} ${styles.dark}`}></div>
+            <div
+                className={
+                    theme === "light"
+                        ? `${styles.toggle} ${styles.light}`
+                        : `${styles.toggle} ${styles.dark}`
+                }
+            ></div>
         </button>
     );
 };
