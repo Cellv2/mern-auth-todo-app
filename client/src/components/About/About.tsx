167d2206f5efc4ee0378175d9760c13497e7faa9
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-05-28 21:07:28 +0100

    Add some content to the about page

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -9,6 +9,29 @@ const About = (props: Props) => {
         <div className={styles.grid}>
             <h1>This is the about page</h1>
             <p>Add a short description of the techs used</p>
+
+            <div className={styles.flexContainer}>
+                <div className={styles.flexItem}>
+                    <img src="http://placehold.it/300" alt="" />
+                    <p>MongoDB</p>
+                </div>
+                <div className={styles.flexItem}>
+                    <img src="http://placehold.it/300" alt="" />
+                    <p>Express.js</p>
+                </div>
+                <div className={styles.flexItem}>
+                    <img src="http://placehold.it/300" alt="" />
+                    <p>React.js</p>
+                </div>
+                <div className={styles.flexItem}>
+                    <img src="http://placehold.it/300" alt="" />
+                    <p>Node.js</p>
+                </div>
+                <div className={styles.flexItem}>
+                    <img src="http://placehold.it/300" alt="" />
+                    <p>TypeScript</p>
+                </div>
+            </div>
         </div>
     );
 };
