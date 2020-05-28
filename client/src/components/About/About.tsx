e1853b4f78a50fb0d0eced4cddba815c3132eee7
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-05-28 21:22:01 +0100

    Fix styling and add MongoDB image

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import mongoDbLogo from "./mongodb-leaf@4x.png";
+
 import styles from "./About.module.scss";
 
 type Props = {};
@@ -12,23 +14,33 @@ const About = (props: Props) => {
 
             <div className={styles.flexContainer}>
                 <div className={styles.flexItem}>
-                    <img src="http://placehold.it/300" alt="" />
+                    <div className={styles.logo}>
+                        <img src={mongoDbLogo} alt="MongoDB Leaf Logo" />
+                    </div>
                     <p>MongoDB</p>
                 </div>
                 <div className={styles.flexItem}>
-                    <img src="http://placehold.it/300" alt="" />
+                    <div className={styles.logo}>
+                        <img src="http://placehold.it/300" alt="" />
+                    </div>
                     <p>Express.js</p>
                 </div>
                 <div className={styles.flexItem}>
-                    <img src="http://placehold.it/300" alt="" />
+                    <div className={styles.logo}>
+                        <img src="http://placehold.it/300" alt="" />
+                    </div>
                     <p>React.js</p>
                 </div>
                 <div className={styles.flexItem}>
-                    <img src="http://placehold.it/300" alt="" />
+                    <div className={styles.logo}>
+                        <img src="http://placehold.it/300" alt="" />
+                    </div>
                     <p>Node.js</p>
                 </div>
                 <div className={styles.flexItem}>
-                    <img src="http://placehold.it/300" alt="" />
+                    <div className={styles.logo}>
+                        <img src="http://placehold.it/300" alt="" />
+                    </div>
                     <p>TypeScript</p>
                 </div>
             </div>
