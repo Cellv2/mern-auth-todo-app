03beb4b0ea45808a44d2fa2cc61aa2b94e882b79
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-05-31 19:04:03 +0100

    Add auth for delete user

--- server/routes/api/users/id/delete-user.ts
+++ server/routes/api/users/id/delete-user.ts
@@ -1,27 +1,53 @@
 import express, { Router, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
 import UserCollection from "../../../../models/User/user-collection.model";
+import { secretOrKey } from "../../../../utils/secrets";
 
 const router: Router = express.Router();
 
 const deleteUser = (req: Request, res: Response): void => {
-    const id: string = "" + req.params.id;
+    if (!req.headers.authorization) {
+        res.sendStatus(401);
 
-    UserCollection.findByIdAndDelete(id, (err, deleted) => {
+        return;
+    }
+
+    const token = req.headers.authorization.split(" ")[1];
+
+    jwt.verify(token, secretOrKey, (err, authorizedData) => {
         if (err) {
-            console.error(err);
             res.sendStatus(500);
 
             return;
         }
 
-        res.statusCode = 200;
-        res.json(deleted);
+        if (!authorizedData) {
+            res.sendStatus(401);
+
+            return;
+        } else {
+            //@ts-expect-error
+            const userId = authorizedData.id;
+
+            UserCollection.findByIdAndDelete(userId, (err, deleted) => {
+                if (err) {
+                    console.error(err);
+                    res.sendStatus(500);
+
+                    return;
+                }
+
+                res.statusCode = 200;
+                res.json(deleted);
+            });
+        }
     });
 
     return;
 };
 
-router.delete("/users/:id/deleteUser", (req: Request, res: Response) => {
+router.delete("/user/deleteUser", (req: Request, res: Response) => {
     deleteUser(req, res);
 });
 
