a40fd91c6b5c58317a8a65f7e5503c9a64577510
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-06-03 22:10:03 +0100

    :construction: Start updating todos for editing

--- client/src/components/ToDos/ToDoItem.tsx
+++ client/src/components/ToDos/ToDoItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./ToDoItem.module.scss";
 
@@ -13,21 +13,88 @@ type Props = {
 
 const ToDoItem = (props: Props) => {
     const { item, index, handleIsCompleteChange, handleDeleteOnClick } = props;
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+    const [inputValue, setInputValue] = useState<string>("");
+
+    // TODO: Pass this up
+    const handleIsEditingOnClick = (): void => {
+        if (isEditing === true) {
+            const newText = inputValue;
+        }
+
+        setIsEditing(!isEditing);
+    };
+
+    const handleInputOnChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ): void => {
+        event.persist();
+        setInputValue(event.target.value);
+    };
+
     return (
-        <div>
+        <>
+            {/* <div>
+                <input
+                    type="checkbox"
+                    name="isComplete"
+                    checked={item.isComplete}
+                    onChange={() => handleIsCompleteChange(index)}
+                />
+                <span className={item.isComplete ? styles.complete : ""}>
+                    {item.text}
+                </span>
+                <button onClick={() => handleDeleteOnClick(index)}>
+                    Click this to delete
+                </button>
+                <br />
+                <br />
+            </div>
+            <div>NEW</div>
             <input
                 type="checkbox"
-                name="isComplete"
-                checked={item.isComplete}
+                name={`isComplete${props.index}`}
+                checked={props.item.isComplete}
                 onChange={() => handleIsCompleteChange(index)}
-            />
-            <span className={item.isComplete ? styles.complete : ""}>
-                {item.text}
-            </span>
-            <button onClick={() => handleDeleteOnClick(index)}>
-                Click this to delete
-            </button>
-        </div>
+            /> */}
+            <div>
+                {!isEditing ? (
+                    <>
+                        <span
+                            className={item.isComplete ? styles.complete : ""}
+                        >
+                            {item.text}
+                        </span>
+                        <button onClick={handleIsEditingOnClick}>Edit</button>
+                        <button onClick={() => handleDeleteOnClick(index)}>
+                            Delete
+                        </button>
+                    </>
+                ) : (
+                    <>
+                        <span>
+                            <input
+                                type="text"
+                                placeholder={item.text}
+                                onChange={handleInputOnChange}
+                                value={inputValue}
+                            />
+                        </span>
+                        <button onClick={handleIsEditingOnClick}>Done</button>
+                    </>
+                )}
+            </div>
+        </>
+    );
+};
+
+const EditableItem = (props: Props) => {
+    return (
+        <input
+            type="checkbox"
+            name={`isComplete${props.index}`}
+            checked={props.item.isComplete}
+        ></input>
     );
 };
 
