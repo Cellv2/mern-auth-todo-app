fbf9a53f3b5e7abe8f587cc6491adf12146ef4ec
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-06-11 00:05:06 +0100

    :recycle: Update component to be functional

--- client/src/components/ToDos/ToDoCreate.tsx
+++ client/src/components/ToDos/ToDoCreate.tsx
@@ -1,69 +1,45 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 type Props = {
     handleCreateOnClick: (inText: string) => void;
 };
-type State = {
-    input: string;
-};
 
-class ToDoCreate extends Component<Props, State> {
-    state = {
-        input: "",
-    };
+const ToDoCreate = (props: Props) => {
+    const { handleCreateOnClick } = props;
+    const [inputValue, setInputValue] = useState<string>("");
 
-    handleInputKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        if (event.key === "Enter" && this.state.input !== "") {
-            this.props.handleCreateOnClick(this.state.input);
-            this.setState({ input: "" });
+    const handleKeyDown = (
+        event: React.KeyboardEvent<HTMLInputElement>
+    ): void => {
+        if (event.key === "Enter" && inputValue !== "") {
+            handleCreateOnClick(inputValue);
+            setInputValue("");
         }
     };
 
-    handleInputOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        event.preventDefault();
-        event.persist();
-
-        this.setState((prevState: State) => ({
-            ...prevState,
-            input: event.target.value,
-        }));
-
-        return;
-    };
-
-    handleSubmitOnClick = (
-        event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-    ) => {
-        event.preventDefault();
-
+    const handleSubmit = (event: React.MouseEvent<HTMLButtonElement>): void => {
         // TODO: Set up hook into a global error state manager so errors can be shown elsewhere?
-        if (this.state.input !== "") {
-            this.props.handleCreateOnClick(this.state.input);
-            this.setState({ input: "" });
+        if (inputValue !== "") {
+            handleCreateOnClick(inputValue);
+            setInputValue("");
         } else {
             console.log("Woah there, you don't have anything in the input!");
         }
-
-        return;
     };
 
-    render() {
-        return (
-            <div>
-                Create an item
-                <input
-                    type="text"
-                    placeholder="Please enter your item text here"
-                    value={this.state.input}
-                    onKeyDown={this.handleInputKeyDown}
-                    onChange={this.handleInputOnChange}
-                />
-                <button onClick={this.handleSubmitOnClick}>
-                    Click this to create
-                </button>
-            </div>
-        );
-    }
-}
+    return (
+        <div>
+            Create an item
+            <input
+                type="text"
+                placeholder="Please enter your item text here"
+                value={inputValue}
+                onKeyDown={handleKeyDown}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button onClick={handleSubmit}>Click this to create</button>
+        </div>
+    );
+};
 
 export default ToDoCreate;
