f0bc8a54c1351e119f190ef6bb780475fe95879c
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-06-19 23:17:08 +0100

    Update footer text

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import styles from "./Footer.module.scss";
 type Props = {};
 
 const Footer = (props: Props) => {
-    return <div className={styles.footer}>This is the footer</div>;
+    return <div className={styles.footer}>Drink the coffee then do the things</div>;
 };
 
 export default Footer;
