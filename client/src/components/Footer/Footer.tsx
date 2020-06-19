220c4110e62767e73ffc7223486321818b2d5daf
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-06-19 23:38:48 +0100

    Add random quotes just because

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+import quotes from "./footer-quotes";
+
 import styles from "./Footer.module.scss";
 
 type Props = {};
 
 const Footer = (props: Props) => {
-    return <div className={styles.footer}>Drink the coffee then do the things</div>;
+    const randomQuoteNumber: number = Math.floor(Math.random() * quotes.length);
+    const randomQuote: string = quotes[randomQuoteNumber];
+    return <div className={styles.footer}>{randomQuote}</div>;
 };
 
 export default Footer;
---
+++ client/src/components/Footer/footer-quotes.ts
@@ -0,0 +1,8 @@
+const quotes: string[] = [
+    "Drink the coffee then do the things",
+    "Just.. DO IT!",
+    "Nobody cares, just work harder",
+    "Well done is better than well said",
+];
+
+export default quotes;
