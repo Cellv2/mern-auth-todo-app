8a74b99366d2553f7ff7d4712f9b3a5f3870e574
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-06-20 22:05:18 +0100

    Formatting

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -9,6 +9,7 @@ type Props = {};
 const Footer = (props: Props) => {
     const randomQuoteNumber: number = Math.floor(Math.random() * quotes.length);
     const randomQuote: string = quotes[randomQuoteNumber];
+
     return <div className={styles.footer}>{randomQuote}</div>;
 };
 
