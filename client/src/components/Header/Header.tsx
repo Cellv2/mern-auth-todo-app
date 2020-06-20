f55961e2f809c1a6ee1a2a3ac1d1978b9a4cb545
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-06-20 22:16:48 +0100

    Cleanup

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -3,14 +3,8 @@ import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
-import NavItem from "react-bootstrap/NavItem";
-import NavLink from "react-bootstrap/NavLink";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Dropdown from "react-bootstrap/Dropdown";
 
-import ProfileDropdown from "./ProfileDropdown";
 import ThemeSwitch from "../ThemeSwitch/ThemeSwitch";
-
 import HeaderProfile from "./HeaderProfile";
 import NavLinks from "./NavLinks";
 
@@ -49,45 +43,6 @@ const Header = (props: Props) => {
                 <HeaderProfile />
             </Navbar>
         </Container>
-        // <>
-        //     <div className={styles.logo}>
-        //         <Link to="/">Home from logo</Link>
-        //     </div>
-        //     <div className={styles.header}>
-        //         This is the header
-        //         <div>
-        //             <nav>
-        //                 <ul className={styles.links}>
-        //                     <li>
-        //                         <Link to="/">Home</Link>
-        //                     </li>
-        //                     <li>
-        //                         <Link to="/about">About</Link>
-        //                     </li>
-        //                     <li>
-        //                         <Link to="/profile">User Profile</Link>
-        //                     </li>
-        //                     <li>
-        //                         <Link to="/register">Register</Link>
-        //                     </li>
-        //                     <li>
-        //                         <Link to="/login">Login</Link>
-        //                     </li>
-        //                     <li>
-        //                         <Link to="/logout">Logout</Link>
-        //                     </li>
-        //                 </ul>
-        //             </nav>
-        //         </div>
-        //     </div>
-        //     <div className={styles.profileDropdown}>
-        //         <ProfileDropdown isAuthenticated={props.isAuthenticated} />
-        //         <ThemeSwitch
-        //             applicationState={props.applicationState}
-        //             handleAppStateUpdate={props.handleAppStateUpdate}
-        //         />
-        //     </div>
-        // </HeaderProfile>
     );
 };
 
