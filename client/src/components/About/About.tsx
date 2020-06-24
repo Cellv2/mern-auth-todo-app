fb0033598892bb132dd55d994c3087d7b5c8c91c
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-06-25 00:50:46 +0100

    Make things a bit more reusable

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -16,174 +16,65 @@ import styles from "./About.module.scss";
 type Props = {};
 
 const About = (props: Props) => {
+    const aboutItems = [
+        {
+            src: mongoDbLogo,
+            alt: "MongoDB Logo",
+            title: "MongoDB title",
+            text: "MongoDB text",
+        },
+        {
+            src: expressLogo,
+            alt: "Express Logo",
+            title: "Express title",
+            text: "Express text",
+        },
+        {
+            src: reactLogo,
+            alt: "React Logo",
+            title: "React title",
+            text: "React text",
+        },
+        {
+            src: nodeJsLogo,
+            alt: "NodeJS Logo",
+            title: "NodeJS title",
+            text: "NodeJS text",
+        },
+        {
+            src: typescriptLogo,
+            alt: "TypeScript Logo",
+            title: "TypeScript title",
+            text: "TypeScript text",
+        },
+    ];
+
     return (
         <div className={styles.gridMain}>
             <h1 className="text-center mb-4">Techs Used</h1>
-            <Container style={{ height: "5rem" }} className="my-3">
-                <Row
-                    //  style={{ height: "100%" }}
-                    className="h-100"
-                >
-                    <Col
-                        // sm={3}
-                        // style={{ height: "100%" }}
-                        className="text-center h-100"
-                    >
-                        <Image
-                            fluid
-                            src={mongoDbLogo}
-                            alt="MongoDB Logo"
-                            className="h-100"
-                            // style={{ maxHeight: "100%" }}
-                        />
-                    </Col>
-                    <Col
-                    //  sm={9}
-                     className="h-100">
-                        <h5>MongoDB title</h5>
-                        <p>MongoDB text</p>
-                    </Col>
-                </Row>
-            </Container>
-            {/*  */}
-            {/*  */}
-            {/*  */}
-            {/* <Container>
-                <Card className="flex-row" style={{backgroundColor: "inherit"}}>
-                    <Col sm={2} className="m-auto">
-
-                        <Image
-                            fluid
-                            src={mongoDbLogo}
-                            alt="MongoDB Logo"
-                            // style={{ maxHeight: "5rem", maxWidth: "5rem" }}
-                            />
-                    </Col>
-                    <Card.Body className="p-0">
-                        <Card.Title>MongoDB title</Card.Title>
-                        <Card.Text>MongoDB text</Card.Text>
-                    </Card.Body>
-                </Card>
-            </Container> */}
-            {/*  */}
-            {/*  */}
-            {/*  */}
-            {/* <Container  className="justify-content-md-center">
-                <Row as={Card}>
-                    <Row>
-                        <Col sm={2}>
-                            <Image fluid src={mongoDbLogo} alt="MongoDB Logo" />
-                        </Col>
-
-                        <Col sm={6}>
-                            <Card.Body>
-                                <Card.Title>MongoDB title</Card.Title>
-                                <Card.Text>MongoDB text</Card.Text>
-                            </Card.Body>
-                        </Col>
-                    </Row>
-                </Row>
-            </Container> */}
-            {/*  */}
-            {/*  */}
-            {/*  */}
-            {/* <Row >
-                <Container>
-                    <Card>
-                        <Row noGutters sm={8}>
-                            <Col sm={2}>
+            {aboutItems.map((item) => {
+                return (
+                    <Container className={`${styles.containerItem} my-3`}>
+                        <Row className="h-100">
+                            <Col className="text-center h-100">
                                 <Image
                                     fluid
-                                    src={mongoDbLogo}
-                                    alt="MongoDB Logo"
+                                    src={item.src}
+                                    alt={item.alt}
+                                    className="h-100"
                                 />
                             </Col>
-
-                            <Col sm={6}>
-                                <Card.Body>
-                                    <Card.Title>MongoDB title</Card.Title>
-                                    <Card.Text>MongoDB text</Card.Text>
-                                </Card.Body>
+                            <Col className="h-100">
+                                <div className="h-100">
+                                    <h5 className="m-0">{item.title}</h5>
+                                    <p className="m-0">{item.text}</p>
+                                </div>
                             </Col>
                         </Row>
-                    </Card>
-                </Container>
-            </Row> */}
-            {/*  */}
-            {/* <Row> */}
-            {/*  */}
-            {/* <Col>
-                    <div>
-                        <div className={styles.logo}>
-                            <img src={mongoDbLogo} alt="MongoDB Leaf Logo" />
-                        </div>
-                        <p>MongoDB</p>
-                    </div>
-                    <div>
-                        <div className={styles.logo}>
-                            <img src={expressLogo} alt="ExpressJS Logo" />
-                        </div>
-                        <p>Express.js</p>
-                    </div>
-                    <div>
-                        <div className={styles.logo}>
-                            <img src={reactLogo} alt="ReactJS Logo" />
-                        </div>
-                        <p>React.js</p>
-                    </div>
-                    <div>
-                        <div className={styles.logo}>
-                            <img src={nodeJsLogo} alt="NodeJS Logo" />
-                        </div>
-                        <p>Node.js</p>
-                    </div>
-                    <div>
-                        <div className={styles.logo}>
-                            <img src={typescriptLogo} alt="TypeScript Logl" />
-                        </div>
-                        <p>TypeScript</p>
-                    </div>
-                </Col> */}
-            {/*  */}
-            {/* </Row> */}
-            {/*  */}
+                    </Container>
+                );
+            })}
         </div>
-        // <div className={styles.grid}>
-        //     <h1>This is the about page</h1>
-        //     <p>Add a short description of the techs used</p>
-        //     <div className={styles.flexContainer}>
-        //         <div className={styles.flexItem}>
-        //             <div className={styles.logo}>
-        //                 <img src={mongoDbLogo} alt="MongoDB Leaf Logo" />
-        //             </div>
-        //             <p>MongoDB</p>
-        //         </div>
-        //         <div className={styles.flexItem}>
-        //             <div className={styles.logo}>
-        //                 <img src={expressLogo} alt="ExpressJS Logo" />
-        //             </div>
-        //             <p>Express.js</p>
-        //         </div>
-        //         <div className={styles.flexItem}>
-        //             <div className={styles.logo}>
-        //                 <img src={reactLogo} alt="ReactJS Logo" />
-        //             </div>
-        //             <p>React.js</p>
-        //         </div>
-        //         <div className={styles.flexItem}>
-        //             <div className={styles.logo}>
-        //                 <img src={nodeJsLogo} alt="NodeJS Logo" />
-        //             </div>
-        //             <p>Node.js</p>
-        //         </div>
-        //         <div className={styles.flexItem}>
-        //             <div className={styles.logo}>
-        //                 <img src={typescriptLogo} alt="TypeScript Logl" />
-        //             </div>
-        //             <p>TypeScript</p>
-        //         </div>
-        //     </div>
-        // </div>
     );
 };
 
