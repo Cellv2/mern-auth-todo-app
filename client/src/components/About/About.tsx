df68110991dddcde96c7d89ac9e06a03e6595335
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-06-25 21:08:02 +0100

    Cleanup

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
 import Image from "react-bootstrap/Image";
 
 import mongoDbLogo from "./mongodb-leaf@4x.png";
