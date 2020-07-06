c9c6b12f993d8b6e8be7a7b411d65ae1a554e227
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-07-06 22:44:11 +0100

    Add username for #53

--- client/src/app/user-slice.ts
+++ client/src/app/user-slice.ts
@@ -5,11 +5,13 @@ import { AvailableThemes } from "../types/theme.types";
 export interface UserState {
     isAuthenticated: boolean;
     theme: AvailableThemes;
+    username: string | null;
 }
 
 export const initialState: UserState = {
     isAuthenticated: false,
     theme: "dark",
+    username: null,
 };
 
 export const userSlice = createSlice({
@@ -22,13 +24,17 @@ export const userSlice = createSlice({
         updateTheme: (state, action: PayloadAction<AvailableThemes>) => {
             state.theme = action.payload;
         },
+        updateUsername: (state, action: PayloadAction<string | null>) => {
+            state.username = action.payload;
+        },
     },
 });
 
-export const { updateAuthenticated, updateTheme } = userSlice.actions;
+export const { updateAuthenticated, updateTheme, updateUsername } = userSlice.actions;
 
 export const themeSelector = (state: RootState) => state.user.theme;
 export const isAuthenticatedSelector = (state: RootState) =>
     state.user.isAuthenticated;
+export const usernameSelector = (state: RootState) => state.user.username;
 
 export default userSlice.reducer;
