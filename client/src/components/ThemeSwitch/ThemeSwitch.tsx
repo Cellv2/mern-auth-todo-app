f07d02ad75a5a35ecbca9e85e22c848a7b2be0f9
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-07-12 23:31:22 +0100

    Cleanup

--- client/src/components/ThemeSwitch/ThemeSwitch.tsx
+++ client/src/components/ThemeSwitch/ThemeSwitch.tsx
@@ -3,17 +3,9 @@ import { useSelector, useDispatch } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 
-import {
-    isAuthenticatedSelector,
-    themeSelector,
-    updateTheme,
-    tokenSelector,
-    updateUserAsync,
-} from "../../app/user-slice";
+import { themeSelector, updateUserAsync } from "../../app/user-slice";
 
 import { AvailableThemes } from "../../types/theme.types";
-import { ApplicationState } from "../../types/application-state.types";
-import { UpdateStateActions } from "../../types/state-action.types";
 
 import styles from "./ThemeSwitch.module.scss";
 import { UserPartial } from "../../types/user.types";
@@ -22,52 +14,20 @@ type Props = {};
 
 const ThemeSwitch = (props: Props) => {
     const theme = useSelector(themeSelector);
-    const token = useSelector(tokenSelector);
-    const isAuthenticated = useSelector(isAuthenticatedSelector);
     const dispatch = useDispatch();
 
     // const { isAuthenticated, theme } = props.applicationState;
     const [initialTheme, setInitialTheme] = useState<AvailableThemes>(theme);
-    const [currentTheme, setCurrentTheme] = useState<AvailableThemes>(theme);
 
     useEffect(() => {
         setInitialTheme(theme);
-        setCurrentTheme(theme);
     }, []);
 
-    useEffect(() => {
-        setCurrentTheme(theme);
-    }, [theme]);
-
     const handleThemeSwitch = async (): Promise<void> => {
-        // const currentTheme = props.applicationState.theme;
         const currentTheme = theme;
         const newTheme: AvailableThemes =
             currentTheme !== "light" ? "light" : "dark";
 
-        // if (isAuthenticated && token !== null) {
-        //     // const token = props.applicationState.user?.token as string;
-        //     const payload = {
-        //         theme: newTheme,
-        //     };
-
-        //     const request = await fetch(`/api/user/profile`, {
-        //         method: "PATCH",
-        //         headers: {
-        //             Authorization: token,
-        //             "Content-Type": "application/json;charset=utf-8",
-        //         },
-        //         body: JSON.stringify(payload),
-        //     });
-
-        //     const response = await request.json();
-        //     console.log(response);
-        // }
-
-        // let newState = props.applicationState;
-        // newState.theme = newTheme;
-
-        // props.handleAppStateUpdate(newState, "updateThemeState");
         const update: UserPartial = { theme: newTheme };
         dispatch(updateUserAsync(update));
 
@@ -82,13 +42,6 @@ const ThemeSwitch = (props: Props) => {
                 size={"3x"}
             />
             <FontAwesomeIcon icon={faSun} className={styles.sun} size={"3x"} />
-            {/* <div
-                className={`
-                ${styles.toggle}
-                ${theme === "light" ? styles.light : styles.dark}
-                ${theme === "light" ? styles.moveLight : ""}
-                `}
-            ></div> */}
             <div
                 className={`
                 ${styles.toggle}
