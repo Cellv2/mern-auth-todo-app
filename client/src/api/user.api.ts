06186d5f28106dd09af90a0255d36d3f7216da34
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-07-13 22:56:28 +0100

    :construction: Add UNTESTED API additions

--- client/src/api/user.api.ts
+++ client/src/api/user.api.ts
@@ -1,4 +1,4 @@
-import { UserPartial } from "../types/user.types";
+import { UserPartial, User } from "../types/user.types";
 
 export const loginUser = async (email: string, password: string) => {
     const body = {
@@ -25,6 +25,63 @@ export const patchUser = async (update: UserPartial, token: string) => {
             "Content-Type": "application/json;charset=utf-8",
         },
         body: JSON.stringify(payload),
+    });
+
+    return request;
+};
+
+//TODO:
+// delete (UserProfile)
+// update PW (UserProfile)
+// create new user (Register)
+
+export const deleteUser = async (token: string) => {
+    const request = await fetch(`/api/user/deleteUser`, {
+        method: "DELETE",
+        headers: {
+            Authorization: token,
+        },
+    });
+
+    return request;
+};
+
+export const updateUserPassword = async (
+    pwOne: string,
+    pwTwo: string,
+    token: string
+) => {
+    const passwordUpdate = { passwordOne: pwOne, passwordTwo: pwTwo };
+
+    const request = await fetch(`/api/user/password/updatePassword`, {
+        method: "PUT",
+        headers: {
+            Authorization: token,
+            "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(passwordUpdate),
+    });
+
+    return request;
+};
+
+export const createUser = async (
+    username: string,
+    email: string,
+    pwOne: string,
+    pwTwo: string
+) => {
+    const newUser = {
+        username: username,
+        email: email,
+        passwordOne: pwOne,
+        passwordTwo: pwTwo,
+    };
+
+    const request = await fetch("/api/users/addUser", {
+        method: "POST",
+        headers: { "Content-Type": "application/json;charset=utf-8" },
+        body: JSON.stringify(newUser),
     });
 
     return request;
