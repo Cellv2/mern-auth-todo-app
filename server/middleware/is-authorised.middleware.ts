d9dd900a97e3d8af0b7efb3dc7e3f6d1406e3cb5
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-07-28 00:00:30 +0100

    Add authorised middleware

--- server/middleware/is-authorised.middleware.ts
+++ server/middleware/is-authorised.middleware.ts
@@ -1,10 +1,44 @@
 import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
-export const myLogger = function (
+import { secretOrKey } from "../../server/utils/secrets";
+import { UserToken } from "../../client/src/types/user.types";
+
+/**
+ * Checks whether the bearer token is present and valid
+ * If it is, it sets res.locals.authorizedData to be the token itself
+ * @returns {UserToken} res.locals.authorizedData
+ */
+export const isAuthorisedMiddleware = function (
     req: Request,
     res: Response,
     next: NextFunction
 ) {
-    console.log("LOGGED");
-    next();
+    if (!req.headers.authorization) {
+        res.sendStatus(401);
+
+        return;
+    }
+
+    const token = req.headers.authorization.split(" ")[1];
+
+    jwt.verify(token, secretOrKey, (err, authorizedData) => {
+        if (err) {
+            res.sendStatus(500);
+
+            return;
+        }
+
+        if (!authorizedData) {
+            res.sendStatus(401);
+
+            return;
+        } else {
+            // TODO: Would be nice to find a way to correctly type this, but it's not the end of the world
+            res.locals.authorizedData = authorizedData as UserToken;
+            return next();
+        }
+    });
+
+    return;
 };
