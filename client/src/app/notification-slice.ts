280f0f0064bf18e5ec8db338ce599024d52237cf
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-08-15 21:33:32 +0100

    Add message validation and reducer for user slice

--- client/src/app/notification-slice.ts
+++ client/src/app/notification-slice.ts
@@ -3,6 +3,7 @@ import { RootState } from "./store";
 
 import { Notification } from "../types/notification.types";
 import { setItemNotification } from "./item-slice";
+import { setUserNotification } from "./user-slice";
 
 export const initialState: Notification = {
     type: null,
@@ -19,11 +20,34 @@ export const notificationSlice = createSlice({
         },
     },
     extraReducers: (builder) =>
-        builder.addCase(setItemNotification, (state, action) => {
-            state.message = action.payload.message;
-            state.type = action.payload.type;
-        }),
+        builder
+            .addCase(setItemNotification, (state, action) => {
+                state.message = action.payload.message;
+                state.type = action.payload.type;
+            })
+            .addCase(setUserNotification, (state, action) => {
+                const { message, type } = action.payload;
+                const validatedMessage = validateMessage(message);
+                state.message = validatedMessage;
+                state.type = type;
+            }),
 });
+
+// TODO: Fix up API returns to actually have a uniform return structure
+// really, we shouldn't need this, but I really should have planned stuff out way better
+const validateMessage = (message: unknown) => {
+    switch (typeof message) {
+        case "string": {
+            return message;
+        }
+        case "object": {
+            return Object.values(message as object)[0];
+        }
+        default: {
+            return String(message);
+        }
+    }
+};
 
 export const { setNotification } = notificationSlice.actions;
 
