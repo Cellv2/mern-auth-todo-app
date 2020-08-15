d1a26f9cba62104af87aaadf798d35e189a4c68d
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-08-15 01:31:58 +0100

    Use notification selectors and update styling

--- client/src/components/Notifications/Toasts.tsx
+++ client/src/components/Notifications/Toasts.tsx
@@ -4,18 +4,39 @@ import Toast from "react-bootstrap/Toast";
 
 import { userErrorSelector } from "../../app/user-slice";
 import { itemErrorSelector } from "../../app/item-slice";
+import { notificationSelector } from "../../app/notification-slice";
+import { NotificationTypes } from "../../types/notification.types";
 
 import "./Toasts.scss"; //not a module because we need to override bootstrap styling
 
 type Props = {};
 
+const cssMapping: { [key in NotificationTypes]: string } = {
+    Error: "errorToast",
+    Success: "successToast",
+};
+
 const Toasts = (props: Props) => {
     const userError = useSelector(userErrorSelector);
     const itemError = useSelector(itemErrorSelector);
+    const notification = useSelector(notificationSelector);
 
-    const [show, setShow] = useState(false);
+    const [show, setShow] = useState(true);
     const [header, setHeader] = useState<string>();
     const [message, setMessage] = useState<string>();
+    const [toastType, setToastType] = useState<string>();
+
+    useEffect(() => {
+        if (!notification || !notification.type || !notification.message) {
+            setShow(false);
+            return;
+        }
+
+        setHeader(notification.type);
+        setMessage(notification.message);
+        setToastType(cssMapping[notification.type]);
+        setShow(true);
+    }, [notification]);
 
     useEffect(() => {
         if (!userError) {
@@ -40,7 +61,7 @@ const Toasts = (props: Props) => {
     }, [itemError]);
 
     return (
-        <div className="errorToast">
+        <div className={toastType ?? "defaultToast"}>
             <Toast
                 show={show}
                 onClose={() => setShow(false)}
