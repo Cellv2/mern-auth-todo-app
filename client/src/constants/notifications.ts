90ae5a32369cad27c5b6c162abe056331a9c91a8
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-08-19 23:54:18 +0100

    :alembic: Testing out 'enums'

--- client/src/constants/notifications.ts
+++ client/src/constants/notifications.ts
@@ -3,7 +3,7 @@ import { Notification } from "../types/notification.types";
 type EnumLiteralsOf<T extends object> = T[keyof T];
 
 export type Notifications = EnumLiteralsOf<typeof Notifications>;
-export const Notifications: { [key: string]: Notification } = Object.freeze({
+export const Notifications = Object.freeze({
     UserUpdateSuccess: {
         type: "Success",
         message: "User successfully updated",
@@ -13,3 +13,21 @@ export const Notifications: { [key: string]: Notification } = Object.freeze({
         message: "User failed to update! Please try again",
     },
 });
+
+type NotificationResponses = "UserUpdateSuccess" | "UserUpdateFailed";
+export type NotificationsTwo = keyof typeof NotificationsTwo;
+export const NotificationsTwo: {
+    [key in NotificationResponses]: Notification;
+} = {
+    UserUpdateSuccess: {
+        type: "Success",
+        message: "User successfully updated",
+    },
+    UserUpdateFailed: {
+        type: "Error",
+        message: "User failed to update! Please try again",
+    },
+} as const;
+
+// var x = Notifications.UserUpdateFailed.message
+// var x = NotificationsTwo.UserUpdateFailed.message
