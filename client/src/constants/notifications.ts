73e464f6a70bc167867704184209ef3ba591cb24
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-08-20 21:51:55 +0100

    Finally made a decision on what to use

--- client/src/constants/notifications.ts
+++ client/src/constants/notifications.ts
@@ -1,8 +1,6 @@
-import { Notification } from "../types/notification.types";
-
 type EnumLiteralsOf<T extends object> = T[keyof T];
 
-export type Notifications = EnumLiteralsOf<typeof Notifications>;
+export type Notifications = EnumLiteralsOf<typeof Notification>;
 export const Notifications = Object.freeze({
     UserUpdateSuccess: {
         type: "Success",
@@ -13,21 +11,3 @@ export const Notifications = Object.freeze({
         message: "User failed to update! Please try again",
     },
 });
-
-type NotificationResponses = "UserUpdateSuccess" | "UserUpdateFailed";
-export type NotificationsTwo = keyof typeof NotificationsTwo;
-export const NotificationsTwo: {
-    [key in NotificationResponses]: Notification;
-} = {
-    UserUpdateSuccess: {
-        type: "Success",
-        message: "User successfully updated",
-    },
-    UserUpdateFailed: {
-        type: "Error",
-        message: "User failed to update! Please try again",
-    },
-} as const;
-
-// var x = Notifications.UserUpdateFailed.message
-// var x = NotificationsTwo.UserUpdateFailed.message
