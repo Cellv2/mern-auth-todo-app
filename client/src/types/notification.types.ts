96dfd890f5a48d523ae91223b44d0b2c030634cc
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-08-22 20:23:15 +0100

    Update type to include heading

--- client/src/types/notification.types.ts
+++ client/src/types/notification.types.ts
@@ -1,6 +1,7 @@
 export type NotificationTypes = "Success" | "Error";
 
-export interface Notification {
+export type Notification = {
     type: NotificationTypes | null;
+    heading?: string | null;
     message: string | null;
 }
