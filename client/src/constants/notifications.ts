81c0b6c006778c1951bad20e3553bd3c3d885012
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-08-23 02:21:03 +0100

    :label: Fix types and add placeholders

--- client/src/constants/notifications.ts
+++ client/src/constants/notifications.ts
@@ -1,9 +1,11 @@
-import { Notification } from '../types/notification.types'
+import { Notification, NotificationActions } from "../types/notification.types";
 type EnumLiteralsOf<T extends object> = T[keyof T];
 
 // these are placeholders, and may be overridden by the response provided by the server
 export type Notifications = EnumLiteralsOf<typeof Notifications>;
-export const Notifications: {[key: string]: Notification} = Object.freeze({
+export const Notifications: {
+    [key in NotificationActions]: Notification;
+} = Object.freeze({
     UserUpdateSuccess: {
         type: "Success",
         heading: "Update success",
@@ -17,81 +19,86 @@ export const Notifications: {[key: string]: Notification} = Object.freeze({
     UserAddSuccess: {
         type: "Success",
         heading: "User created",
-        message: "User created successfully, please sign in"
+        message: "User created successfully, please sign in",
     },
     UserAddFailed: {
         type: "Error",
         heading: "User failed to create",
-        message: "User creation failed! Please enter details and try again"
+        message: "User creation failed! Please enter details and try again",
     },
     UserLoginSuccess: {
         type: "Success",
         heading: "Success",
-        message: "User login successful"
+        message: "User login successful",
     },
     UserLoginFailed: {
         type: "Error",
         heading: "Login failed",
-        message: "Login failed! Please enter details and try again"
+        message: "Login failed! Please enter details and try again",
     },
     UserDeleteSuccess: {
         type: "Success",
         heading: "User deleted",
-        message: "User delete successful"
+        message: "User delete successful",
     },
     UserDeleteFailed: {
         type: "Error",
         heading: "User delete failed",
-        message: "User delete failed! Please try again"
+        message: "User delete failed! Please try again",
     },
     UserPasswordUpdateSuccess: {
         type: "Success",
         heading: "Password updated",
-        message: "Your password has been updated"
+        message: "Your password has been updated",
     },
     UserPasswordUpdateFailed: {
         type: "Error",
         heading: "Password update failed",
-        message: "Your password was not updated! Please try again"
+        message: "Your password was not updated! Please try again",
     },
     ItemAddToDbSuccess: {
         type: "Success",
         heading: "Item added",
-        message: "Item added successfully"
+        message: "Item added successfully",
     },
     ItemAddToDbFailed: {
         type: "Error",
         heading: "Item add failed",
-        message: "Item creation was not successful! Please try again"
+        message: "Item creation was not successful! Please try again",
     },
     ItemDeleteFromDbSuccess: {
         type: "Success",
         heading: "Item deleted",
-        message: "Item deleted successfully"
+        message: "Item deleted successfully",
     },
     ItemDeleteFromDbFailed: {
         type: "Error",
         heading: "Item delete failed",
-        message: "Item failed to delete! Please try again"
+        message: "Item failed to delete! Please try again",
     },
     ItemUpdateInDbSuccess: {
         type: "Success",
         heading: "Item updated",
-        message: "Item updated successfully"
+        message: "Item updated successfully",
     },
     ItemUpdateInDbFailed: {
         type: "Error",
         heading: "Item update failed",
-        message: "Item failed to update! Please reload and try again"
+        message: "Item failed to update! Please reload and try again",
     },
     ItemFetchFromDbSuccess: {
         type: "Success",
         heading: "Item retreived",
-        message: "Item retreived successfully"
+        message: "Item retreived successfully",
     },
     ItemFetchFromDbFailed: {
         type: "Error",
         heading: "Item retreive failed",
-        message: "Item was not retreived! Please reload the page"
+        message: "Item was not retreived! Please reload the page",
+    },
+    Server500: {
+        type: "Error",
+        heading: "Error 500",
+        message: "Internal Server Error, please try again later",
     },
 });
--- client/src/types/notification.types.ts
+++ client/src/types/notification.types.ts
@@ -2,6 +2,27 @@ export type NotificationTypes = "Success" | "Error";
 
 export type Notification = {
     type: NotificationTypes | null;
-    heading?: string | null;
+    heading: string | null;
     message: string | null;
-}
+};
+
+export type NotificationActions =
+    | "UserUpdateSuccess"
+    | "UserUpdateFailed"
+    | "UserAddSuccess"
+    | "UserAddFailed"
+    | "UserLoginSuccess"
+    | "UserLoginFailed"
+    | "UserDeleteSuccess"
+    | "UserDeleteFailed"
+    | "UserPasswordUpdateSuccess"
+    | "UserPasswordUpdateFailed"
+    | "ItemAddToDbSuccess"
+    | "ItemAddToDbFailed"
+    | "ItemDeleteFromDbSuccess"
+    | "ItemDeleteFromDbFailed"
+    | "ItemUpdateInDbSuccess"
+    | "ItemUpdateInDbFailed"
+    | "ItemFetchFromDbSuccess"
+    | "ItemFetchFromDbFailed"
+    | "Server500";
